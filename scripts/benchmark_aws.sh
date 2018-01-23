beacc0ea8efa84b36e3ecd078103a211eb2e70c2
Sanjiv Krish <sanjiv436.k@gmail.com>
2018-01-23 12:11:05 +0100

    Add scripts to run benchmark
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

---
+++ scripts/benchmark_aws.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p $HOME/logs
+count=3
+
+while [ $count -lt 10 ]
+do
+	sudo sysbench --test=../../usr/share/sysbench/oltp_read_only.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_read_only.log
+	sudo sysbench --test=../../usr/share/sysbench/oltp_read_write.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_read_write.log
+	sudo sysbench --test=../../usr/share/sysbench/oltp_write_only.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_write_only.log
+	sudo sysbench --test=../../usr/share/sysbench/oltp_insert.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_insert.log
+	sudo sysbench --test=../../usr/share/sysbench/oltp_delete.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_delete.log
+	sudo sysbench --test=../../usr/share/sysbench/select_random_points.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/select_random_points.log
+	sudo sysbench --test=../../usr/share/sysbench/select_random_ranges.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/select_random_ranges.log
+	sudo sysbench --test=../../usr/share/sysbench/oltp_update_index.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_update_index.log
+	sudo sysbench --test=../../usr/share/sysbench/oltp_update_non_index.lua --mysql-user=root --mysql-password=sanjiv436 --mysql-db=dbname --mysql-host=ecinstance.cu57np2yl7m4.us-west-2.rds.amazonaws.com --db-driver=mysql --threads=$((2**count)) run >> $HOME/logs/oltp_update_non_index.log
+	count=$((count + 1))
+done
